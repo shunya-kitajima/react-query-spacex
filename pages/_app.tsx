8e134a3dc6d6d25b97e121f14dd2aac1eb3a8d0b
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-09-20 12:57:35 +0900

    modify

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { QueryClient, QueryClientProvider } from 'react-query'
-import { ReactQueryDevtools } from 'react-query-devtools'
+import { ReactQueryDevtools } from 'react-query/devtools'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   const queryClient = new QueryClient()
